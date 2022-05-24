13b346b8d7dc543a963e3083ef00e8d4c5cbb4ac
lsauthie <ludovicsauthier@gmail.com>
2022-05-24 13:59:27 +0200

    small changes

--- base.py
+++ base.py
@@ -3,12 +3,26 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 
 load_dotenv()
 
 dbuser = os.getenv('DBUSER')
 dbpass = os.getenv('DBPASS')
 dbserver = os.getenv('DBSERVER')
+
+#if you have reached step3 - uncomment this block
+keyVaultName = os.environ["KEY_VAULT_NAME"]
+
+KVUri = "https://{}.vault.azure.net".format(keyVaultName)
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+secretName = 'secret-sauce'
+dbpass = client.get_secret(secretName) #if keyvault is configured, credential is overwritten from value in keyvault
+print(retrieved_secret.value)
+# --- end keyvault
 
 if dbserver != 'localhost':#running on azure
     connstr = 'postgresql://{}:{}@{}.postgres.database.azure.com/swissre-db?sslmode=require'.format(dbuser, dbpass, dbserver)
